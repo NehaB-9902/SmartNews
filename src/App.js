f2a7671c152d133945b355103ad89794ccf467f5
Neha Bharti <109811363+NehaB-9902@users.noreply.github.com>
2022-07-22 23:01:15 +0530

    initialize project using create react app

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { useState } from 'react';
+
+function App () {
+  const pagesize=8;
+  const [progress,setProgress] = useState(0)
+    return (
+      <div>
+        <BrowserRouter>
+        <Navbar/>
+        <LoadingBar
+          color='#f11946'
+          height={3}
+          progress={progress}
+        />
+        <Routes>
+            <Route exact path="/" element={<News setProgress={setProgress}   key="general" pagesize={pagesize} country="in" category="general"/>}></Route>
+            <Route exact path="/business" element={<News setProgress={setProgress}   key="business" pagesize={pagesize} country="in" category="business"/>}></Route>
+            <Route exact path="/entertainment" element={<News setProgress={setProgress}    key="entertainment" pagesize={pagesize} country="in" category="entertainment"/>}></Route>
+            <Route exact path="/general" element={<News setProgress={setProgress}    key="general" pagesize={pagesize} country="in" category="general"/>}> </Route>
+            <Route exact path="/health" element={<News setProgress={setProgress}   key="health" pagesize={pagesize} country="in" category="health"/>}> </Route>
+            <Route exact path="/science" element={<News setProgress={setProgress}    key="science" pagesize={pagesize} country="in" category="science"/>}> </Route>
+            <Route exact path="/sports" element={<News setProgress={setProgress}   key="sports" pagesize={pagesize} country="in" category="sports"/>}> </Route>
+            <Route exact path="/technology" element={<News setProgress={setProgress}   key="technology" pagesize={pagesize} country="in" category="technology"/>}></Route>
+        </Routes>
+        </BrowserRouter>
+      </div>
+    )
+}
+
+export default App
